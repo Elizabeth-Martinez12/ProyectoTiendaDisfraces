c0b46b6651b3e42acc5dd197d8b706e3641553c6
Elizabeth <elizamartlo121102@gmail.com>
2023-12-06 20:56:31 -0600

    Proyecto

--- procesar_agregar_usuario.php
+++ procesar_agregar_usuario.php
@@ -1,9 +1,5 @@
 <?php
-// procesar_agregar_perfil.php
-
-// Verificar si se recibieron los datos del formulario
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Conectar a la base de datos
     $servidor = "localhost";
     $usuario = "root";
     $contrasena = "";
@@ -16,13 +12,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         die("La conexión a la base de datos ha fallado: " . mysqli_connect_error());
     }
 
-    // Obtener datos del formulario
     $username = $_POST["username"];
     $password = $_POST["password"];
     $status = $_POST["status"];
     $profile_id = $_POST["profile_id"];
 
-    // Insertar nuevo perfil en la base de datos
     $sql = "INSERT INTO $tabla (username, password, status, profile_id) VALUES ('$username', '$password', '$status', '$profile_id')";
 
     if (mysqli_query($conn, $sql)) {
@@ -31,13 +25,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         echo "Error al agregar usuario: " . mysqli_error($conn);
     }
 
-    // Cerrar la conexión
     mysqli_close($conn);
 } else {
     echo "Acceso no autorizado.";
 }
 
-// Redirigir a la vista de perfiles después de procesar el formulario
 header("Location: Usuarios.php");
 exit();
 ?>
